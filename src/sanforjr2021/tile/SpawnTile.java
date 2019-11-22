ec440ef0a3a30183ec6ab3f617d3b0777bb9e31c
sanforjr2021 <sanforjr2021@users.noreply.github.com>
2019-11-21 23:20:19 -0500

    Added Framework for Extra Tiles

---
+++ src/sanforjr2021/tile/SpawnTile.java
@@ -0,0 +1,13 @@
+package sanforjr2021.tile;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SpawnTile extends Tile{
+
+    public SpawnTile(Integer x, Integer y) {
+        super(x, y);
+    }
+    public void spawnEnemy() throws NotImplementedException {
+        //todo: add code to spawn enemy
+    }
+}
---
+++ src/sanforjr2021/tile/Tower.java
@@ -0,0 +1,8 @@
+package sanforjr2021.tile;
+
+public class Tower extends Tile {
+
+    public Tower(Integer x, Integer y) {
+        super(x, y);
+    }
+}
